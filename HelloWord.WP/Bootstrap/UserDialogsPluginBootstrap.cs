bf930fb0f6cd7917891471f6a2c18813ddd7b4a3
Geir Sagberg <geir.sagberg@gmail.com>
2014-09-22 21:25:12 +0200

    Removed apps

--- HelloWord.WP/Bootstrap/UserDialogsPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace HelloWord.WP.Bootstrap
-{
-    public class UserDialogsPluginBootstrap
-        : MvxPluginBootstrapAction<Acr.MvvmCross.Plugins.UserDialogs.PluginLoader>
-    {
-    }
-}
