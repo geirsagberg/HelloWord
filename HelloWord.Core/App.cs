8d711d4a5d096c5ec514e56e02c7b1a6f4658b9a
Geir Sagberg <geir.sagberg@gmail.com>
2015-04-28 20:02:05 +0200

    Added iOS and Android.

---
+++ HelloWord.Core/App.cs
@@ -0,0 +1,17 @@
+using Cirrious.CrossCore.IoC;
+
+namespace HelloWord.Core
+{
+    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+				
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
